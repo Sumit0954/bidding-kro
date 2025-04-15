5d7ec20d064197a6ac20f1627d69c8451c4b3a13
sumit-appcode <sumit.singh@appcodeindia.com>
2025-04-15 17:44:11 +0530

    admin's Query API's integration

--- src/components/admin/query/QueryDetail.jsx
+++ src/components/admin/query/QueryDetail.jsx
@@ -6,10 +6,57 @@ import {
   Divider,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./QueryDetail.module.scss";
+import _sendAPIRequest from "../../../helpers/api";
+import { useParams } from "react-router-dom";
+import { AdminApiUrls } from "../../../helpers/api-urls/AdminApiUrls";
+import { AlertContext } from "../../../contexts/AlertProvider";
+import { dateTimeFormatter } from "../../../helpers/formatter";
+import ScreenLoader from "../../../elements/CustomScreeenLoader/ScreenLoader";
 
 const QueryDetail = () => {
+  const { query_type, query_id } = useParams();
+  const { setAlert } = useContext(AlertContext);
+  const [queryDetails, setQueryDetails] = useState({});
+  const [screenLoader, setScreenLoader] = useState(true);
+
+  const fetchUserQueryDetail = async () => {
+    let API;
+    if (query_type === "contact-us") {
+      API = AdminApiUrls.RETRIEVE_CONTACT_US_QUERY_DETAIL;
+    } else if (query_type === "get-in-touch") {
+      API = AdminApiUrls.RETRIEVE_GET_IN_TOUCH_QUERY_DETAIL;
+    } else if (query_type === "missing-data-query") {
+      API = AdminApiUrls.RETRIEVE_MISSING_DATA_QUERY_DETAIL;
+    }
+    try {
+      const response = await _sendAPIRequest(
+        "GET",
+        `${API}${query_id}/`,
+        "",
+        true
+      );
+      if (response.status === 200) {
+        setQueryDetails(response.data);
+        setScreenLoader(false);
+      }
+    } catch (error) {
+      setAlert({
+        isVisible: true,
+        message: "Something went wrong",
+        severity: "error",
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchUserQueryDetail();
+  }, [query_type, query_id]);
+
+  if (screenLoader) {
+    return <ScreenLoader />;
+  }
   return (
     <>
       <Accordion
@@ -26,40 +73,88 @@ const QueryDetail = () => {
           <div className="row">
             <div className="col">
               <h6 className={styles["col-heading"]}>Query No.</h6>
-              <p className={styles["col-data"]}></p>
+              <b className={styles["col-data"]}>Query {queryDetails?.id}</b>
             </div>
             <div className="col">
               <h6 className={styles["col-heading"]}>Query Type</h6>
-              <p className={styles["col-data"]}></p>
+              <p className={styles["col-data"]}>
+                {query_type === "contact-us"
+                  ? "Contact Us Query"
+                  : query_type === "get-in-touch"
+                  ? "Demo Query"
+                  : query_type === "missing-data-query"
+                  ? `Missing Data Query`
+                  : "Unknown Type"}
+              </p>
             </div>
             <div className="col">
-              <h6 className={styles["col-heading"]}>Query Date</h6>
-              <p className={styles["col-data"]}></p>
+              <h6 className={styles["col-heading"]}>Query raised Date</h6>
+              <p className={styles["col-data"]}>
+                {dateTimeFormatter(queryDetails?.created_at)}
+              </p>
             </div>
           </div>
           <Divider classes={{ root: "custom-divider" }} />
           <div className="row">
             <div className="col">
-              <h6 className={styles["col-heading"]}>User Name</h6>
-              <p className={styles["col-data"]}></p>
+              <h6 className={styles["col-heading"]}>Company Name</h6>
+              <p className={styles["col-data"]}>
+                {query_type === "contact-us"
+                  ? queryDetails?.name
+                  : query_type === "get-in-touch"
+                  ? queryDetails?.company_name
+                  : query_type === "missing-data-query"
+                  ? queryDetails?.company?.name
+                  : ""}
+              </p>
             </div>
             <div className="col">
               <h6 className={styles["col-heading"]}>User Email</h6>
-              <p className={styles["col-data"]}></p>
+              <a
+                className={styles["col-data"]}
+                href={`mailto:${
+                  query_type === "missing-data-query"
+                    ? queryDetails?.company?.business_email
+                    : queryDetails?.email
+                }`}
+              >
+                {query_type === "missing-data-query"
+                  ? queryDetails?.company?.business_email
+                  : queryDetails?.email}
+              </a>
             </div>
             <div className="col">
-              <h6 className={styles["col-heading"]}>User Mobile</h6>
-              <p className={styles["col-data"]}></p>
-            </div>
-          </div>
-          <Divider classes={{ root: "custom-divider" }} />
+              <h6 className={styles["col-heading"]}>
+                {{
+                  "contact-us": "Subject",
+                  "get-in-touch": "Contact Person Name",
+                  "missing-data-query": "Type",
+                }[query_type] || ""}
+              </h6>
 
-          <div className="row">
-            <div className="col">
-              <h6 className={styles["col-heading"]}>Message</h6>
-              <p className={styles["col-data"]}></p>
+              <p className={styles["col-data"]}>
+                {query_type === "contact-us"
+                  ? queryDetails?.subject
+                  : query_type === "get-in-touch"
+                  ? queryDetails?.contact_person_name
+                  : query_type === "missing-data-query"
+                  ? queryDetails?.type
+                  : ""}
+              </p>
             </div>
           </div>
+          {query_type !== "get-in-touch" && (
+            <>
+              <Divider classes={{ root: "custom-divider" }} />
+
+              <div className="row">
+                <div className="col">
+                  <h6 className={styles["col-heading"]}>Message</h6>
+                  <p className={styles["col-data"]}>{queryDetails?.message}</p>
+                </div>
+              </div>
+            </>
+          )}
         </AccordionDetails>
       </Accordion>
     </>
--- src/components/admin/query/QueryList.jsx
+++ src/components/admin/query/QueryList.jsx
@@ -1,24 +1,130 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import DataTable from "../../../elements/CustomDataTable/DataTable";
-import { queries_column } from "../../../elements/CustomDataTable/AdminColumnData";
-import { TableCell } from "@mui/material";
+import { Box, Tab, TableCell, Tabs } from "@mui/material";
+import _sendAPIRequest from "../../../helpers/api";
+import { AdminApiUrls } from "../../../helpers/api-urls/AdminApiUrls";
+import { AlertContext } from "../../../contexts/AlertProvider";
+import PropTypes from "prop-types";
+import {
+  contact_us_queries_column,
+  get_in_touch_queries_column,
+  missing_data_queries_column,
+} from "../../../elements/CustomDataTable/AdminColumnData";
 
 const QueryList = () => {
+  const [value, setValue] = useState(0);
+  const { setAlert } = useContext(AlertContext);
+  const [tableColumns, setTableColumns] = useState([]);
+  const [tableData, setTableData] = useState([]);
+
   const addAction = (cell) => {
     return (
       <TableCell {...cell.getCellProps()}> {cell.render("Cell")} </TableCell>
     );
   };
+  const handleTabChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const fetchUserQueries = async (value) => {
+    let API;
+
+    switch (value) {
+      case 0:
+        API = AdminApiUrls.CONTACT_US_QUERY;
+        setTableColumns(contact_us_queries_column);
+        break;
+      case 1:
+        API = AdminApiUrls.GET_IN_TOUCH_QUERY;
+        setTableColumns(get_in_touch_queries_column);
+        break;
+      case 2:
+        API = AdminApiUrls.MISSING_DATA_QUERY;
+        setTableColumns(missing_data_queries_column);
+        break;
+      default:
+        console.log("Invalid API value");
+        return;
+    }
+
+    try {
+      const response = await _sendAPIRequest("GET", API, "", true);
+      if (response.status === 200) {
+        setTableData(response.data);
+      }
+    } catch (error) {
+      setAlert({
+        isVisible: true,
+        message: "Something went wrong",
+        severity: "error",
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchUserQueries(value);
+  }, [value]);
   return (
     <>
-      <DataTable
-        propsColumn={queries_column}
-        propsData={[]}
-        action={addAction}
-        customClassName="admin-data-table"
-      />
+      <Box sx={{ width: "100%" }}>
+        <Tabs
+          value={value}
+          onChange={handleTabChange}
+          aria-label="query-list-tabs"
+          variant="scrollable"
+          scrollButtons="auto"
+          sx={{
+            "@media (max-width: 600px)": {
+              width: "100%", // Tabs take full width only on mobile
+              marginBottom: "1rem", // Adds spacing below tabs for mobile
+            },
+          }}
+        >
+          <Tab label="Contact Us" {...a11yProps(0)} />
+          <Tab label="Get In Touch" {...a11yProps(1)} />
+          <Tab label="Missing Data Query" {...a11yProps(2)} />
+        </Tabs>
+
+        <Box sx={{ mt: 2 }}>
+          <DataTable
+            propsColumn={tableColumns}
+            propsData={tableData}
+            action={addAction}
+            customClassName="admin-data-table"
+          />
+        </Box>
+      </Box>
     </>
   );
 };
 
 export default QueryList;
+
+const TabPanel = (props) => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && children}
+    </div>
+  );
+};
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
